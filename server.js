19c769cdf54662a2b2db35779d5577dcbb6f9fe7
Mohamed Alhosary <malhosary328@gmail.com>
2025-07-14 02:30:27 +0300

    fix: add fallback port for production

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+
 
 app.use(cors());
 app.use(express.json());
